1b67a865bda79dc1f467191a303904756dca51bb
Mario Zechner <badlogicgames@gmail.com>
2023-11-27 13:43:48 +0100

    Dockerization for dev & deployment.

--- publish.sh
+++ publish.sh
@@ -5,11 +5,11 @@ current_date=$(date "+%Y-%m-%d %H:%M:%S")
 commit_hash=$(git rev-parse HEAD)
 echo "{\"date\": \"$current_date\", \"commit\": \"$commit_hash\"}" > html/version.json
 
-rsync -avz --exclude node_modules --exclude .git --exclude data --exclude repos.json ./ badlogic@marioslab.io:/home/badlogic/skychat.social/app
+rsync -avz --exclude node_modules --exclude .git --exclude data --exclude docker/data --exclude repos.json ./ badlogic@marioslab.io:/home/badlogic/skychat.social/app
 
 if [ "$1" == "server" ]; then
     echo "Publishing client & server"
-    ssh -t marioslab.io "cd skychat.social && ./reload.sh && docker-compose logs -f"
+    ssh -t marioslab.io "cd skychat.social && ./docker/control.sh stop && ./docker/control.sh start && ./docker/control.sh logs"
 else
     echo "Publishing client only"
 fi
